482332ab4b2f5b101a724ad20c80fccea8051257
Xinyi Zhang <xzhang515@cs.ucsb.edu>
2020-02-02 01:01:16 -0800

    hz - added controller method for multiplication results

--- src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
@@ -112,5 +112,23 @@ public class OperationsController {
         model.addAttribute("ratCalcForm", ratCalcForm);
         return "operations/multiply";
     }
+    
+    @GetMapping("/multiply/results")
+    public String getMultiplyResult(Model model, @Valid RatCalcForm ratCalcForm, BindingResult bindingResult) {
+        logger.info("getMultiplyResult ratCalcForm=" + ratCalcForm);
+        ratCalcForm.setOp("-");
+        
+        if (!bindingResult.hasErrors() &&!checkDenominatorErrors(ratCalcForm)) {
+            Rational r1 = new Rational(ratCalcForm.getNum1(), ratCalcForm.getDenom1());
+            Rational r2 = new Rational(ratCalcForm.getNum2(), ratCalcForm.getDenom2());
+            Rational result = Rational.product(r1, r2);
+            logger.info("r1=" + r1 + " r2=" + r2 + " result=" + result);
+            ratCalcForm.setNumResult(result.getNumerator());
+            ratCalcForm.setDenomResult(result.getDenominator());
+        }
+        
+        model.addAttribute("ratCalcForm", ratCalcForm);
+        return "operations/multiply";
+    }
 
 }
